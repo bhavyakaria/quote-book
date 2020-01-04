7155f263caf92723f33576597057bd3ab1027c42
bhavyakaria <bhavyakaria@yahoo.in>
2020-01-05 02:51:52 +0530

    added default first book

--- src/app/quote-board/quote-board.component.ts
+++ src/app/quote-board/quote-board.component.ts
@@ -11,7 +11,6 @@ export class QuoteBoardComponent implements OnInit {
 
   constructor(private data: DataService) { }
 
-  listOfBooks = new Set();
   list = [];
   showList = [];
   listEmitter = new BehaviorSubject<any[]>(this.showList);
@@ -24,14 +23,16 @@ export class QuoteBoardComponent implements OnInit {
 
     this.data.bookName.subscribe(val => {
       this.showList = [];
-      let x = val;
       this.list.forEach(obj => {
-        console.log(val['book_name'] == obj['book_name']);
+
+        if (!val) {
+          val = this.list[0]['book_name'];
+        }
+
         if (val === obj['book_name']) {
           this.showList.push(obj);
         }
       });
-      console.log(this.showList);
       this.listEmitter.next(this.showList);
     });
   }
