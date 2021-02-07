883e3c8a90df0a1cfb2b04bc969932cb4e0cc5ee
bhavyakaria <bhavyakaria@yahoo.in>
2021-02-08 01:35:12 +0530

    feat: added a progress bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
 import { Router } from '@angular/router';
 import { ApiRequestService } from '../utils/api-request.service';
 import { BookResponse } from '../models/interfaces';
+
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-home',
@@ -14,12 +15,14 @@ export class HomeComponent implements OnInit {
   listOfBooks: any[];
 
   constructor(private router: Router,
-    private apiRequestService: ApiRequestService) { }
+    private apiRequestService: ApiRequestService,
+    private ngxLoader: NgxUiLoaderService) { }
 
   ngOnInit() {
+    this.ngxLoader.start();
     this.apiRequestService.fetchBooks().subscribe((res: BookResponse) => {
       this.listOfBooks = res.books;
-
+      this.ngxLoader.stop();
       if (this.listOfBooks.length == 0) {
         this.router.navigateByUrl('/file-upload');
       }
