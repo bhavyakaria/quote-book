2e48297961cf121be3c627a3188ffccf2475c0fd
bhavyakaria <bhavyakaria@yahoo.in>
2024-04-05 01:17:50 +0530

    feat: added highlights component
    Signed-off-by bhavyakaria <bhavyakaria@yahoo.in>

--- src/pages/dashboard/Highlights.jsx
+++ src/pages/dashboard/Highlights.jsx
@@ -17,21 +17,39 @@ const Highlights = () => {
     <>
       <div className="highlights">
         <div className="left-section">
-          <div className="search m-4 flex max-w-sm">
-            <input className="h-12 max-h-12 w-full px-4 py-1 text-base" type="search" placeholder="Search" />
+          <div className="search flex max-w-sm bg-white bg-clip-border p-4">
+            <input
+              className="h-12 max-h-12 w-full px-4 py-1 text-base"
+              id="search"
+              type="search"
+              placeholder="Search"
+            />
           </div>
           <div className="book-list">
             {highlights &&
-              highlights.map((highlight) => (
-                <div key={highlight.id} className="book-container flex-row items-center justify-center text-center">
+              highlights.map(highlight => (
+                <div
+                  key={highlight.id}
+                  className="book-container flex-row items-center justify-center text-center"
+                >
                   <div className="cover"></div>
                   <div className="book-name mx-4">{highlight.bookName}</div>
-                  <div className="book-author mx-4 my-1">{highlight.author}</div>
+                  <div className="book-author mx-4 my-1">
+                    {highlight.author}
+                  </div>
                 </div>
               ))}
           </div>
         </div>
-        <div className="right-section">Test</div>
+        <div className="right-section">
+          <div className="quote-list mt-6">
+            {highlights &&
+              highlights.map(highlight => (
+                <QuoteCard quote={highlight.highlight} />
+              ))}
+          </div>
+          <BookRecommendCard />
+        </div>
       </div>
     </>
   );
