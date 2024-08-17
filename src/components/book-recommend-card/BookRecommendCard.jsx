d93f0597d323621541bc7b1af470157093641531
bhavyakaria <bhavyakaria@yahoo.in>
2024-08-18 00:07:33 +0530

    feat: added book recommendation card

--- src/components/book-recommend-card/BookRecommendCard.jsx
+++ src/components/book-recommend-card/BookRecommendCard.jsx
@@ -1,17 +1,28 @@
-import BulbIcon from "../../static/icons/bulb-icon.svg";
-import BookCard from "../book-card/BookCard";
-import "./BookRecommendCard.scss";
+import BulbIcon from '../../static/icons/bulb-icon.svg';
+import BookCard from '../book-card/BookCard';
+import './BookRecommendCard.scss';
 
 const BookRecommendCard = () => {
   return (
-    <div className="book-recommend-card relative mt-6 flex flex-col rounded-md bg-white bg-clip-border p-4 text-gray-700">
+    <div className="book-recommend-card relative mt-6 flex flex-col rounded-xl bg-white bg-clip-border p-4 text-gray-700">
       <div className="header flex justify-between">
-        <span className="text-base font-semibold">Similar Books You Might Like</span>
+        <span className="text-base font-semibold">
+          Similar Books You Might Like
+        </span>
         <img src={BulbIcon} alt="bulb-icon" />
       </div>
-      <BookCard bookName={"The Intelligent Investor"} bookAuthor={"Benjamin Graham"} />
-      <BookCard bookName={"The Intelligent Investor"} bookAuthor={"Benjamin Graham"} />
-      <BookCard bookName={"The Intelligent Investor"} bookAuthor={"Benjamin Graham"} />
+      <BookCard
+        bookName={'The Intelligent Investor'}
+        bookAuthor={'Benjamin Graham'}
+      />
+      <BookCard
+        bookName={'The Intelligent Investor'}
+        bookAuthor={'Benjamin Graham'}
+      />
+      <BookCard
+        bookName={'The Intelligent Investor'}
+        bookAuthor={'Benjamin Graham'}
+      />
     </div>
   );
 };
