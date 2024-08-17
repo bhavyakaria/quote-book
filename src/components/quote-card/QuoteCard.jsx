074f758e7dfdd37d284787040d8d5cf2b1b2a391
bhavyakaria <bhavyakaria@yahoo.in>
2024-08-18 00:09:20 +0530

    chore: refactored code

--- src/components/quote-card/QuoteCard.jsx
+++ src/components/quote-card/QuoteCard.jsx
@@ -1,14 +1,14 @@
-import HeartIcon from "../../static/icons/heart-icon.svg";
-import TagIcon from "../../static/icons/tag-icon.svg";
-import ShareIcon from "../../static/icons/share-icon.svg";
-import DeleteIcon from "../../static/icons/delete-icon.svg";
-import "./QuoteCard.scss";
+import HeartIcon from '../../static/icons/heart-icon.svg';
+import TagIcon from '../../static/icons/tag-icon.svg';
+import ShareIcon from '../../static/icons/share-icon.svg';
+import DeleteIcon from '../../static/icons/delete-icon.svg';
+import './QuoteCard.scss';
 
-const QuoteCard = ({ quote }) => {
+const QuoteCard = ({ quote, index }) => {
   return (
-    <div className="quote-card relative mt-6 flex flex-col rounded-md bg-white bg-clip-border p-8 text-gray-700">
+    <div className="quote-card relative mt-6 flex flex-col rounded-xl bg-white bg-clip-border p-8 text-gray-700" key={index}>
       <div className="text-base">{quote}</div>
-      <div className="icons flex space-x-8 self-end p-4">
+      <div className="icons flex space-x-8 self-end p-4 pb-0">
         <img src={HeartIcon} alt="fav-icon" />
         <img src={TagIcon} alt="fav-icon" />
         <img src={ShareIcon} alt="fav-icon" />
